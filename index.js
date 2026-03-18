5558892b679b59a64ca377dab7ae86a7d51dc2fa
HoKoMaH <Anwahilali@gmail.com>
2026-03-18 04:17:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ app.get("/stats", async (req, res) => {
         </style></head>
         <body>
             <div style="max-width: 900px; margin: auto; text-align:center;">
-                <h1>📊 لوحة تحكم AR.SA</h1>
+                <h1>📊 لوحة التحكم بالأضافة</h1>
                 <a href="${installUrl}" style="background:#8e44ad; color:white; padding:12px 25px; border-radius:50px; text-decoration:none; font-weight:bold; display:inline-block; margin-bottom:20px;">+ تثبيت في Stremio</a>
                 
                 <div class="stats-grid">
